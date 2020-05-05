698dd87e75d1d2865ae134f987165c010ad4a068
yeon_hong <shine_koko@naver.com>
2020-05-05 16:28:08 +0900

    yh second commit

--- app/src/main/java/com/example/crofo_app/MainActivity.java
+++ app/src/main/java/com/example/crofo_app/MainActivity.java
@@ -2,16 +2,24 @@ package com.example.crofo_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.PointF;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.LinearLayout;
+import android.widget.Toast;
+import android.content.Context;
 
 import com.skt.Tmap.TMapData;
+import com.skt.Tmap.TMapMarkerItem;
 import com.skt.Tmap.TMapPoint;
 import com.skt.Tmap.TMapPolyLine;
 import com.skt.Tmap.TMapTapi;
 import com.skt.Tmap.TMapView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,7 +35,6 @@ public class MainActivity extends AppCompatActivity {
         linearLayoutTmap.addView( tMapView );
 
         System.out.println("안뇽\n");
-
         TMapPoint startPoint = new TMapPoint(37.570841, 126.985302);            //SKT타워
         TMapPoint endPoint = new TMapPoint(37.551135, 126.988205);              //N서울타워
         Navigation naviTest = new Navigation(startPoint, endPoint, tMapView);
@@ -35,5 +42,37 @@ public class MainActivity extends AppCompatActivity {
 
         tMapView.setCenterPoint(126.988205, 37.551135);
 
+        // 연홍's코드 수정ing
+        TMapMarkerItem markerItem1 = new TMapMarkerItem();
+        TMapPoint tMapPoint1 = new TMapPoint(37.570841, 126.985302); // SKT타워
+        Context context = null;
+        context = this;
+        Bitmap bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.poi_dot);
+
+        markerItem1.setIcon(bitmap); // 마커 아이콘 지정
+        markerItem1.setPosition(0.5f, 1.0f); // 마커의 중심점을 중앙, 하단으로 설정
+        markerItem1.setTMapPoint( tMapPoint1 ); // 마커의 좌표 지정
+        markerItem1.setName("SKT타워"); // 마커의 타이틀 지정
+        tMapView.addMarkerItem("markerItem1", markerItem1); // 지도에 마커 추가
+
+        // 현재위치로 표시되는 좌표의 위도, 경도 반환
+        // double latitude = tMapPoint1.getLatitude();
+        // double Longitude = tMapPoint1.getLongitude();
+
+
+        // 클릭 이벤트 설정
+        tMapView.setOnClickListenerCallBack(new TMapView.OnClickListenerCallback(){
+            @Override
+            public boolean onPressEvent(ArrayList arrayList, ArrayList arrayList1, TMapPoint tMapPoint, PointF pointF) {
+                //Toast.makeText(MapEvent.this, "onPress~!", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+
+            @Override
+            public boolean onPressUpEvent(ArrayList arrayList, ArrayList arrayList1, TMapPoint tMapPoint, PointF pointF) {
+                //Toast.makeText(MapEvent.this, "onPressUp~!", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
     }
 }
