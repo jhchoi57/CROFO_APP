8107c89c5c94b73c98a063dd2461bc6e881bcb07
jhchoi57 <enough1125@gmail.com>
2020-05-16 13:49:18 +0900

    메인 레이아웃 constrain으로 변경, CrossAlert 클래스, tts추가

--- app/src/main/java/com/example/crofo_app/MainActivity.java
+++ app/src/main/java/com/example/crofo_app/MainActivity.java
@@ -1,18 +1,15 @@
 package com.example.crofo_app;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Color;
 import android.graphics.PointF;
 import android.location.Location;
 import android.location.LocationListener;
@@ -20,20 +17,15 @@ import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
-import android.util.Log;
 import android.view.View;
-import android.view.Window;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 import android.content.Context;
 
-import com.skt.Tmap.TMapData;
 import com.skt.Tmap.TMapGpsManager;
 import com.skt.Tmap.TMapMarkerItem;
 import com.skt.Tmap.TMapPoint;
-import com.skt.Tmap.TMapPolyLine;
-import com.skt.Tmap.TMapTapi;
 import com.skt.Tmap.TMapView;
 
 import java.util.ArrayList;
@@ -71,139 +63,41 @@ public class MainActivity extends AppCompatActivity implements TMapGpsManager.on
 
         super.onCreate(savedInstanceState);
 
-        // 세로모드고정
+        // 세로모드고정, activity_main View 띄우기
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-
-        // activity_main View 띄우기
         setContentView(R.layout.activity_main);
 
-
-
-        // 위치권한 탐색 허용 관련
-        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)!=PackageManager.PERMISSION_GRANTED) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-            }
-            return;
-        }
+        // 위치권한 탐색 허용 관련, 현재 위치 받아오기
+        acceptLocationPermission();
         setGps();
+        receiveLocation();
 
+        //tts 한국어로 초기화, TMAP 띄우기
+        initTTS();
+        setTMAP();
 
-        // 현재 위치 받아오기
-        gps = new TMapGpsManager(MainActivity.this);
-        gps.setMinTime(1000);
-        gps.setMinDistance(2);
-        // 현재 위치 인터넷으로 받기 gps 면 GPS_PROVIDER
-        gps.setProvider(gps.NETWORK_PROVIDER);
-        gps.OpenGps();
-
-        //tts 한국어로 초기화
-        tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
-            @Override
-            public void onInit(int status) {
-                if(status != ERROR) {
-                    // 언어를 선택한다.
-                    tts.setLanguage(Locale.KOREAN);
-                }
-            }
-        });
-
-
-        LinearLayout linearLayoutTmap = (LinearLayout)findViewById(R.id.linearLayoutTmap);
-        tMapView = new TMapView(this);
-
-        tMapView.setSKTMapApiKey( "l7xx57390f539bd74175a4783fe65224453e" );
-        linearLayoutTmap.addView( tMapView );
-
-//        startPoint = new TMapPoint(37.570841, 126.985302);            //SKT타워
-//        endPoint = new TMapPoint(37.551135, 126.988205);              //N서울타워
-//        Navigation naviTest = new Navigation(startPoint, endPoint, tMapView);
-//        naviTest.execute(startPoint, endPoint);
-
-
-        // 시작 중심 좌표
-        // 시작 현위치로
+        // 시작 중심 좌표, 시작 현위치로
         tMapView.setTrackingMode(true);
         Toast.makeText(getApplicationContext(), "현재 위치 : " + tMapView.getLocationPoint(), Toast.LENGTH_LONG).show();
-        //tMapView.setCenterPoint(gps.getLocation().getLongitude(), gps.getLocation().getLatitude());
 
-        // 연홍's코드 수정ing
-        Context context = this;
-        Bitmap bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.poi_dot);
-
-        markerItem1.setIcon(bitmap); // 마커 아이콘 지정
-//        markerItem1.setPosition(0.5f, 1.0f); // 마커의 중심점을 중앙, 하단으로 설정
-//        markerItem1.setTMapPoint( tMapMarkerPoint ); // 마커의 좌표 지정
-//        markerItem1.setName("SKT타워"); // 마커의 타이틀 지정
-//        tMapView.addMarkerItem("markerItem1", markerItem1); // 지도에 마커 추가
-
-        // 버튼 ID 찾기
-        btnStarting = (Button)findViewById(R.id.btnStart);
-        btnDestination = (Button)findViewById(R.id.btnEnd);
-        btnFinish = (Button)findViewById(R.id.btnFinish);
-        btnCurrentLocationToStarting = (Button)findViewById(R.id.btnCurrentLocationToStarting);
-
-        // 버튼 Listener
-        btnStarting.setOnClickListener(mClickListener);
-        btnDestination.setOnClickListener(mClickListener);
-        btnFinish.setOnClickListener(mClickListener);
-        btnCurrentLocationToStarting.setOnClickListener(mClickListener);
-
-        // tMapview 클릭 이벤트
-        tMapView.setOnClickListenerCallBack(new TMapView.OnClickListenerCallback(){
-            @Override
-            public boolean onPressEvent(ArrayList arrayList, ArrayList arrayList1, TMapPoint tMapPoint, PointF pointF) {
-                return false;
-            }
-
-            @Override
-            public boolean onPressUpEvent(ArrayList arrayList, ArrayList arrayList1, TMapPoint tMapPoint, PointF pointF) {
-                return false;
-            }
-        });
-
-        // tMapview 롱 클릭 이벤트
-        tMapView.setOnLongClickListenerCallback(new TMapView.OnLongClickListenerCallback() {
-            @Override
-            public void onLongPressEvent(ArrayList arrayList, ArrayList arrayList1, TMapPoint tMapPoint) {
-                tMapMarkerPoint = tMapPoint;
-                markerItem1.setTMapPoint( tMapMarkerPoint ); // 마커의 좌표 지정
-                markerItem1.setName(""); // 마커의 타이틀 지정
-                tMapView.addMarkerItem("markerItem1", markerItem1); // 지도에 마커 추가
-                btnStarting.setVisibility(View.VISIBLE);
-                btnDestination.setVisibility(View.VISIBLE);
-                //btnFinish.setVisibility(View.VISIBLE);
-            }
-        });
-
-
+        // 마커 아이콘 지정, 버튼 설정, tMapView 클릭 이벤트
+        setMarkerIcon();
+        setButton();
+        tMapViewClickEvent();
+        tMapViewLongClickEvent();
 
         // 커스텀 다이얼로그를 호출한다.
         CrossFrame crossFrame = new CrossFrame(MainActivity.this);
         crossFrame.callFunction();
 
-        startTimerTask();
+        //startTimerTask();
     }
 
     // onCreate 끝
 
+    //함수들 시작
 
-
-    public void startTimerTask(){
-        timerTask = new TimerTask(){
-            @Override
-            public void run(){
-                Toast.makeText(getApplicationContext(),"런런런", Toast.LENGTH_LONG).show();
-            }
-        };
-
-        Timer timer = new Timer();
-        timer.schedule(timerTask,5000);
-    }
-
-
-    // btnStart, btnEnd 클릭 이벤트
+    // 버튼 이벤트
     Button.OnClickListener mClickListener = new View.OnClickListener(){
         public void onClick(View v){
             switch(v.getId()){
@@ -211,13 +105,6 @@ public class MainActivity extends AppCompatActivity implements TMapGpsManager.on
                 case R.id.btnStart :
                     startPoint = tMapMarkerPoint;
                     Toast.makeText(getApplicationContext(), "위도 : " + startPoint.getLatitude() + "\n경도 : " + startPoint.getLongitude(), Toast.LENGTH_LONG).show();
-
-                    // 도착지가 0 0 이 아니면 네비게이션 시작
-//                    if(endPoint.getLongitude() != 0 && endPoint.getLatitude() != 0){
-//                        Navigation navigation = new Navigation(startPoint, endPoint, tMapView);
-//                        navigation.execute(startPoint, endPoint);
-//                        btnFinish.setVisibility(View.VISIBLE);
-//                    }
                     break;
 
                 // 도착지 설정 버튼
@@ -277,17 +164,113 @@ public class MainActivity extends AppCompatActivity implements TMapGpsManager.on
                     tMapView.setCenterPoint(startPoint.getLongitude(), startPoint.getLatitude());
                     Toast.makeText(getApplicationContext(), "위도 : " + startPoint.getLatitude() + "\n경도 : " + startPoint.getLongitude(), Toast.LENGTH_LONG).show();
 
-                    // 도착지가 0 0 이 아니면 네비게이션 시작
-//                    if(endPoint.getLongitude() != 0 && endPoint.getLatitude() != 0){
-//                        Navigation navigation = new Navigation(startPoint, endPoint, tMapView);
-//                        navigation.execute(startPoint, endPoint);
-//                        btnFinish.setVisibility(View.VISIBLE);
-//                    }
                     break;
 
             }
         }
     };
+
+    public void setMarkerIcon(){
+        Context context = this;
+        Bitmap bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.poi_dot);
+        markerItem1.setIcon(bitmap);
+    }
+
+    public void tMapViewClickEvent(){
+        tMapView.setOnClickListenerCallBack(new TMapView.OnClickListenerCallback(){
+            @Override
+            public boolean onPressEvent(ArrayList arrayList, ArrayList arrayList1, TMapPoint tMapPoint, PointF pointF) {
+                return false;
+            }
+
+            @Override
+            public boolean onPressUpEvent(ArrayList arrayList, ArrayList arrayList1, TMapPoint tMapPoint, PointF pointF) {
+                return false;
+            }
+        });
+    }
+
+    public void tMapViewLongClickEvent(){
+        tMapView.setOnLongClickListenerCallback(new TMapView.OnLongClickListenerCallback() {
+            @Override
+            public void onLongPressEvent(ArrayList arrayList, ArrayList arrayList1, TMapPoint tMapPoint) {
+                tMapMarkerPoint = tMapPoint;
+                markerItem1.setTMapPoint( tMapMarkerPoint ); // 마커의 좌표 지정
+                markerItem1.setName(""); // 마커의 타이틀 지정
+                tMapView.addMarkerItem("markerItem1", markerItem1); // 지도에 마커 추가
+                btnStarting.setVisibility(View.VISIBLE);
+                btnDestination.setVisibility(View.VISIBLE);
+                //btnFinish.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+
+    public void acceptLocationPermission(){
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)!=PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }
+            return;
+        }
+    }
+
+    public void receiveLocation(){
+        // 현재 위치 받아오기
+        gps = new TMapGpsManager(MainActivity.this);
+        gps.setMinTime(1000);
+        gps.setMinDistance(2);
+        // 현재 위치 인터넷으로 받기 gps 면 GPS_PROVIDER
+        gps.setProvider(gps.NETWORK_PROVIDER);
+        gps.OpenGps();
+    }
+
+
+    public void startTimerTask(){
+        timerTask = new TimerTask(){
+            @Override
+            public void run(){
+                Toast.makeText(getApplicationContext(),"런런런", Toast.LENGTH_LONG).show();
+            }
+        };
+
+        Timer timer = new Timer();
+        timer.schedule(timerTask,5000);
+    }
+
+    public void setButton(){
+        // 버튼 ID 찾기
+        this.btnStarting = (Button)findViewById(R.id.btnStart);
+        this.btnDestination = (Button)findViewById(R.id.btnEnd);
+        this.btnFinish = (Button)findViewById(R.id.btnFinish);
+        this.btnCurrentLocationToStarting = (Button)findViewById(R.id.btnCurrentLocationToStarting);
+
+        // 버튼 Listener
+        this.btnStarting.setOnClickListener(mClickListener);
+        this.btnDestination.setOnClickListener(mClickListener);
+        this.btnFinish.setOnClickListener(mClickListener);
+        this.btnCurrentLocationToStarting.setOnClickListener(mClickListener);
+    }
+
+    public void initTTS(){
+        tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != ERROR) {
+                    // 언어를 선택한다.
+                    tts.setLanguage(Locale.KOREAN);
+                }
+            }
+        });
+    }
+
+    public void setTMAP(){
+        LinearLayout linearLayoutTmap = (LinearLayout)findViewById(R.id.linearLayoutTmap);
+        tMapView = new TMapView(this);
+
+        tMapView.setSKTMapApiKey( "l7xx57390f539bd74175a4783fe65224453e" );
+        linearLayoutTmap.addView( tMapView );
+    }
 
     private final LocationListener mLocationListener = new LocationListener() {
         public void onLocationChanged(Location location) {
@@ -295,12 +278,8 @@ public class MainActivity extends AppCompatActivity implements TMapGpsManager.on
             if (location != null) {
                 double latitude = location.getLatitude();
                 double longitude = location.getLongitude();
-                //tMapView.setLocationPoint(longitude, latitude); // 현재위치로 표시될 좌표의 위도, 경도를 설정합니다.
-                //tMapView.setIconVisibility(true);
-                //tmap.setCenterPoint(longitude, latitude,true); // 현재 위치로 이동
                 System.out.println("TmapTest" +  Double.toString(longitude));
             }
-
         }
 
         public void onProviderDisabled(String provider) {
