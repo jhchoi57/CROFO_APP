75439af04574d88809d2d094f5a654a8a1ff23aa
yeon_hong <shine_koko@naver.com>
2020-05-26 21:30:21 +0900

    yh drawimgFunction / img update

--- app/src/main/java/com/example/crofo_app/CrossFrame.java
+++ app/src/main/java/com/example/crofo_app/CrossFrame.java
@@ -2,70 +2,88 @@ package com.example.crofo_app;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.graphics.Color;
 import android.view.Gravity;
 import android.view.Window;
 import android.view.WindowManager;
 import android.view.WindowManager.LayoutParams;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class CrossFrame {
     private Context context;
-
     public CrossFrame(Context context) {
         this.context = context;
     }
+    public Dialog frontdlg = null;
+    public Dialog rightdlg = null;
 
     // 호출할 다이얼로그 함수를 정의한다.
     public void callCrossFront() {
 
         // 커스텀 다이얼로그를 정의하기위해 Dialog클래스를 생성한다.
-        final Dialog dlg = new Dialog(context);
+        frontdlg = new Dialog(context);
 
         // 액티비티의 타이틀바를 숨긴다.
-        dlg.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        frontdlg.requestWindowFeature(Window.FEATURE_NO_TITLE);
 
         // 커스텀 다이얼로그의 레이아웃을 설정한다.
-        dlg.setContentView(R.layout.crossframefront);
+        frontdlg.setContentView(R.layout.crossframefront);
 
         // 커스텀 다이얼로그 위치 조정
-        LayoutParams params = dlg.getWindow().getAttributes();
+        LayoutParams params = frontdlg.getWindow().getAttributes();
         // params.x = 100;
         // params.y = 500;
         // params.width = 300;
         // params.height = 300;
-        dlg.getWindow().setAttributes(params);
-        dlg.getWindow().setGravity(Gravity.CENTER);
+        frontdlg.getWindow().setAttributes(params);
+        frontdlg.getWindow().setGravity(Gravity.CENTER);
 
         // 배경 어두워지는거 없애기
-        dlg.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+        frontdlg.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
 
         // 커스텀 다이얼로그를 노출한다
-        dlg.show();
+        frontdlg.show();
+    }
+
+    public void drawObjFront(int left, int top, int obj){
+        ImageView iv = new ImageView(context);
+
+        // 오브젝트 받아서 if로 나누면 댐
+        iv.setImageResource(R.drawable.person);
+        LinearLayout.LayoutParams param = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        param.width = 70;
+        param.height = 70;
+        param.setMargins(left,top,0,0);
+        frontdlg.addContentView(iv, param);
     }
 
     public void callCrossRight() {
 
         // 커스텀 다이얼로그를 정의하기위해 Dialog클래스를 생성한다.
-        final Dialog dlg = new Dialog(context);
+        rightdlg = new Dialog(context);
 
         // 액티비티의 타이틀바를 숨긴다.
-        dlg.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        rightdlg.requestWindowFeature(Window.FEATURE_NO_TITLE);
 
         // 커스텀 다이얼로그의 레이아웃을 설정한다.
-        dlg.setContentView(R.layout.crossframeright);
+        rightdlg.setContentView(R.layout.crossframeright);
 
         // 커스텀 다이얼로그 위치 조정
-        LayoutParams params = dlg.getWindow().getAttributes();
+        LayoutParams params = rightdlg.getWindow().getAttributes();
         // params.x = 500;
         // params.y = 100;
         // params.width = 300;
         // params.height = 300;
-        dlg.getWindow().setAttributes(params);
-        dlg.getWindow().setGravity(Gravity.TOP | Gravity.RIGHT);
+        rightdlg.getWindow().setAttributes(params);
+        rightdlg.getWindow().setGravity(Gravity.TOP | Gravity.RIGHT);
 
         // 배경 어두워지는거 없애기
-        dlg.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+        rightdlg.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
 
         // 커스텀 다이얼로그를 노출한다
-        dlg.show();
+        rightdlg.show();
     }
+
+
 }
--- app/src/main/java/com/example/crofo_app/MainActivity.java
+++ app/src/main/java/com/example/crofo_app/MainActivity.java
@@ -101,6 +101,8 @@ public class MainActivity extends AppCompatActivity implements TMapGpsManager.on
         CrossFrame crossFrame = new CrossFrame(MainActivity.this);
         crossFrame.callCrossFront();
         crossFrame.callCrossRight();
+        crossFrame.drawObjFront(0,0, 1);
+        crossFrame.drawObjFront(400,200, 1);
 
         // 현재 위치 타이머로 5초마다 계속 얻기
         //tMapView.setTrackingMode(true);
