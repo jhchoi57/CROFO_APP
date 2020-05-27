e73922bd7b6a507bc6bc20ca3c124b0496f3adcd
jhchoi57 <enough1125@gmail.com>
2020-05-28 03:45:52 +0900

    safetydrive 함수 추가

--- app/src/main/java/com/example/crofo_app/SafetyDrive.java
+++ app/src/main/java/com/example/crofo_app/SafetyDrive.java
@@ -11,6 +11,7 @@ import com.skt.Tmap.TMapMarkerItem;
 import com.skt.Tmap.TMapPoint;
 import com.skt.Tmap.TMapView;
 
+import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -67,5 +68,91 @@ public class SafetyDrive extends AsyncTask<TMapPoint, Void, Void> {
         bitmap = bitmap.createScaledBitmap(bitmap,100,100,true);
         markerItemCurrent.setIcon(bitmap);
     }
+
+    public double getTrueBearing(double[] point1, double[] point2){
+        // point[0] = lat , point[1] = lon
+
+        double[] point1_radian = new double[2];
+        double[] point2_radian = new double[2];
+
+        // 현재 위치 : 위도나 경도는 지구 중심을 기반으로 하는 각도이기 때문에 라디안 각도로 변환한다.
+        point1_radian[0] = point1[0] * (3.141592 / 180);        //lat
+        point1_radian[1] = point1[1] * (3.141592 / 180);        //lon
+
+        // 목표 위치 : 위도나 경도는 지구 중심을 기반으로 하는 각도이기 때문에 라디안 각도로 변환한다.
+        point2_radian[0] = point2[0] * (3.141592 / 180);        //lat
+        point2_radian[1] = point2[1] * (3.141592 / 180);        //lon
+
+        // radian distance
+
+        double radian_distance = 0;
+
+        radian_distance = Math.acos(Math.sin(point1_radian[0]) * Math.sin(point2_radian[0]) + Math.cos(point1_radian[0]) * Math.cos(point2_radian[0]) * Math.cos(point1_radian[1] - point2_radian[1]));
+
+
+
+        // 목적지 이동 방향을 구한다.(현재 좌표에서 다음 좌표로 이동하기 위해서는 방향을 설정해야 한다. 라디안값이다.
+
+        double radian_bearing = Math.acos((Math.sin(point2_radian[0]) - Math.sin(point1_radian[0]) * Math.cos(radian_distance)) / (Math.cos(point1_radian[0]) * Math.sin(radian_distance)));
+        // acos의 인수로 주어지는 x는 360분법의 각도가 아닌 radian(호도)값이다.
+
+        double true_bearing = 0;
+
+        if (Math.sin(point2_radian[1] - point1_radian[1]) < 0)
+
+        {
+
+            true_bearing = radian_bearing * (180 / 3.141592);
+
+            true_bearing = 360 - true_bearing;
+
+        }
+
+        else
+
+        {
+
+            true_bearing = radian_bearing * (180 / 3.141592);
+
+        }
+
+        return true_bearing;
+
+    }
+
+    // ROI 안에 들어왔는지
+    public boolean isInPolygon(ArrayList<double[]> roi, double[] point){
+        //crosses는 점q와 오른쪽 반직선과 다각형과의 교점의 개수
+        int crosses = 0;
+
+        for(int i = 0 ; i < 4 ; i++){
+            int j = (i+1)%4;
+            //점 B가 선분 (p[i], p[j])의 y좌표 사이에 있음
+            if((roi.get(i)[1] > point[1]) != (roi.get(j)[1] > point[1]) ){
+                //atX는 점 B를 지나는 수평선과 선분 (p[i], p[j])의 교점
+                double atX = (roi.get(j)[0] - roi.get(i)[0]) * (point[1] - roi.get(i)[1]) / (roi.get(j)[1] - roi.get(i)[1]) + roi.get(i)[0];
+                //atX가 오른쪽 반직선과의 교점이 맞으면 교점의 개수를 증가시킨다.
+                if(point[0] < atX)
+                    crosses++;
+            }
+        }
+        return crosses % 2 > 0;
+    }
+
+    // 2개 이상이면 현재 차량 기준으로 필터링
+    public double[] ifHaveManyROI(double trueBearing, ArrayList<double[]> roi, double[] curLocation){
+        int index = 0;
+        double min = 360;
+        for(int i = 0;i<roi.size();i++){
+            double diff = Math.abs(getTrueBearing(curLocation, roi.get(i)) - trueBearing);
+            if(min > diff){
+                index = i;
+                min = diff;
+            }
+        }
+        return roi.get(index);
+    }
+
+    
 
 }
